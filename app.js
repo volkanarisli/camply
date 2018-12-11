76960e78fd9db3e29e4dd4abe70152b58b2df6a7
Volkan Arıslı <volkanarisli@gmail.com>
2018-12-11 16:52:06 +0300

    :sparkles: Deploye on Heroku

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ var express = require("express"),
     app = express(),
     bodyParser =require("body-parser"),
     mongoose = require("mongoose"),
+    flash    =require("connect-flash"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
     Campground = require("./models/campground"),
     Comment    = require("./models/comment"),
     User       = require("./models/user"),
     seedDB     = require("./seeds");
-    
+ //requaring routes
+var commentRoutes           = require("./routes/comments"),
+    campgroundRoutes        = require("./routes/campgrounds"),
+    indexRoutes              = require("./routes/index")
  
-mongoose.connect("mongodb://localhost/yeld_camp");
+ 
+// var url = process.env.DATABASEURL || "mongodb://localhost/yeld_camp";
+// mongoose.connect(url,{ useNewUrlParser: true });
+
+mongoose.connect("mongodb://volkan:volk0198@ds251332.mlab.com:51332/yeldcamp",{ useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs")
 app.use(express.static(__dirname+"/public")); 
-   
-   
-   seedDB(); 
+app.use(methodOverride("_method"));   
+app.use(flash());
+   //seed the database
+ //seedDB(); 
    
    
 
@@ -36,144 +46,21 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req,res,next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 });
 
-
-app.get("/",function (req,res) {
-res.render("landing");
-});
-
+app.use("/campgrounds/:id/comments",commentRoutes);
+app.use("/campgrounds",campgroundRoutes);
+app.use("/",indexRoutes);
 
 
-app.get("/campgrounds",function (req,res) {
-      
-    
-    
-    
-        Campground.find({},function(err,allCampgrounds) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("campgrounds/index",{campgrounds:allCampgrounds});
-            }
-        });
-});
-
-app.get("/campgrounds/new",function(req, res) {
-   res.render("campgrounds/new"); 
-});
-app.post("/campgrounds",function(req,res) {
- var name = req.body.name;
- var image = req.body.image;
- var description = req.body.description;
- var newCampground = {name : name,image : image,description: description};
- 
- Campground.create(newCampground,function (err,newlyCreated) {
-     if (err) {
-         console.log(err)
-     } else {
-         res.redirect("/campgrounds")
-     }
- });
- 
- });
- //Show route
- app.get("/campgrounds/:id",function(req, res) {
-    Campground.findById( req.params.id).populate("comments").exec(function (err,foundCampground) {
-       if (err) {
-           console.log(err);
-       } else {
-           res.render("campgrounds/show",{campground:foundCampground});
-       }
-    });
-     
- });
-
-//===========================
-//COMMNETS ROUTES
-//===========================
-
-app.get("/campgrounds/:id/comments/new",isLoggedIn,function(req, res) {
-   //find campground by id
-   Campground.findById(req.params.id,function (err,campground) {
-       if (err) {
-           console.log(err)
-       } else {
-             res.render("comments/new",{campground:campground});
-       }
-   })
-  
-});
-
-app.post("/campgrounds/:id/comments",isLoggedIn,function (req,res) {
-   Campground.findById(req.params.id,function(err, campground) {
-       if (err) {
-           console.log(err);
-           res.redirect("/campgrounds");
-       } else {
-           Comment.create(req.body.comment,function (err,comment) {
-               if (err) {
-                   console.log(err);
-               } else {
-                   campground.comments.push(comment);
-                   campground.save();
-                   res.redirect("/campgrounds/"+ campground._id);
-               }
-           });
-       }
-   });
-});
-//============
-//AUTH ROUTES
-//============
-//SHOW REGISTER FORM
-
-app.get("/register",function(req, res) {
-   res.render("register"); 
-});
-//handle sign up logic
-app.post("/register",function(req, res) {
-   var newUser = new User({username : req.body.username});
-    User.register(newUser,req.body.password,function (err,user) {
-        if (err) {
-            console.log(err);
-            return res.render("register");
-        } else {
-            passport.authenticate("local")(req,res,function(){
-               res.redirect("/campgrounds");
-            });
-        }
-    });
-});
-//show login form
-
-app.get("/login",function(req, res) {
-    res.render("login");
-});
-//login logic
-app.post("/login",passport.authenticate("local",
-        {
-            successRedirect:"/campgrounds" ,
-            failueRedirect: "/login"
-            
-        }),function(req, res) {
-    
-});
-//logout route
-app.get("/logout",function(req, res) {
-    req.logout();
-    res.redirect("/campgrounds");
-});
 
 
-function isLoggedIn(req,res,next) {
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-};
 
-app.listen(process.env.PORT,process.env.IP,function () {
-    console.log("The YeldCamp Server Has Started");
+
+
+app.listen(process.env.PORT||8000,function () {
+    console.log("The Camply Server Has Started");
 });
--- models/campground.js
+++ models/campground.js
@@ -2,8 +2,16 @@ var mongoose = require("mongoose");
  
 var campgroundSchema = new mongoose.Schema({
    name: String,
+   price: String,
    image: String,
    description: String,
+   author :{
+      id:{
+         type: mongoose.Schema.Types.ObjectId,
+         ref:  "User"
+      },
+      username:String
+   },
    comments: [
       {
          type: mongoose.Schema.Types.ObjectId,
